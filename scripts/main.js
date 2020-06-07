d11cc5dcb9d3b1098c3bfe92b62e896115fbe23b
chamcham10324 <10324yuu@gmail.com>
2020-06-07 16:23:16 +0900

    'add'

---
+++ scripts/main.js
@@ -0,0 +1,65 @@
+'use strict';
+
+{
+    window.addEventListener('keydown', (e) => {
+        console.log(e.key);
+    })
+
+    const subject = document.getElementById('subject');
+    const form = document.forms.typing;
+    const timer = document.getElementById('timer');
+    const textList = [
+        'Hello World',
+        'Good',
+        'I love JavaScript',
+        'This is MyApp',
+        'Welcome'
+    ];
+
+    let TIME = 20;
+    let count = 0;
+    let state = true;
+
+    // カウントダウン機能 functionの処理を1000ミリ秒ごとに繰り返す
+    const countdown = setInterval(function () {
+        timer.textContent = 'Time Limit : ' + --TIME + 'second';
+        if (TIME <= 0) finish();
+    }, 1000);
+
+
+    // フォーム処理(正解不正解の判定，点数の処理)
+    form.btn.addEventListener('click', function (e) {
+        if (!state) return;
+
+        if (form.input.value === subject.textContent) {
+            count++;
+            document.getElementById('score').textContent = count;
+            init();
+        } else {
+            subject.textContent = '間違いです！';
+            setTimeout(function () { init() }, 1000)
+        }
+    });
+
+    init();
+
+
+    // 問題文をランダムに設定する処理
+    function init() {
+        const rnd = Math.floor(Math.random() * textList.length);
+
+        
+        subject.textContent = textList[rnd];
+        form.input.value = '';
+        form.input.focus();
+    }
+
+    // タイピング結果の表示
+    function finish() {
+        clearInterval(countdown);
+        subject.textContent = 'SCORE : ' + count;
+        // ゲームの終了処理
+        state = false;
+    }
+
+}
